b6a88cb665fe3ee263724565aa1786d926578714
Rendel69 <94101216+Rendel69@users.noreply.github.com>
2023-09-24 20:01:42 +0700

    Add files via upload

---
+++ IdeaProjects/untitled/src/zad4.java
@@ -0,0 +1,8 @@
+public class zad4 {
+    public static void main(String[] args) {
+        double h = 10;
+
+        double t = Math.sqrt((2 * h) / 9.8);
+        System.out.println("Время падения тела с высоты " + h + " метров равно " + t + " секунд");
+    }
+}
---
+++ IdeaProjects/untitled/src/zad5.java
@@ -0,0 +1,7 @@
+public class zad5 {
+    public static void main (String[] args){
+        int a=40,c=45;
+        double b=Math.sqrt(c*c-a*a);
+        System.out.println("Вывод катета "+ (float) b);
+    }
+}
