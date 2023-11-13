03b050fbf5bda8d0751e6938f7477cbc2f11cfcd
Rendel69 <94101216+Rendel69@users.noreply.github.com>
2023-11-13 23:33:31 +0700

    Delete IdeaProjects/untitled/src directory

--- IdeaProjects/untitled/src/zad5.java
+++
@@ -1,7 +0,0 @@
-public class zad5 {
-    public static void main (String[] args){
-        int a=40,c=45;
-        double b=Math.sqrt(c*c-a*a);
-        System.out.println("Вывод катета "+ (float) b);
-    }
-}
